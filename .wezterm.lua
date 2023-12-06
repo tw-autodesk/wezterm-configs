cebc06c165d6513a8cdc89053a7c840f3a7d2a2c
Tyler Wilson <tyler.wilson@autodesk.com>
2023-12-06 14:26:24 -0600

    initial commit

---
+++ .wezterm.lua
@@ -0,0 +1,64 @@
+local w = require("wezterm")
+local config = w.config_builder()
+
+-- color scheme
+
+config.color_scheme = "tokyonight_night"
+
+-- constants
+
+local direction_keys = {
+  Left = "h",
+  Down = "j",
+  Up = "k",
+  Right = "l",
+  -- reverse lookup
+  h = "Left",
+  j = "Down",
+  k = "Up",
+  l = "Right",
+}
+
+-- helper functions
+
+local function is_vim(pane)
+  return pane:get_user_vars().IS_NVIM == "true"
+end
+
+local function split_nav(resize_or_move, key)
+  return {
+    key = key,
+    mods = resize_or_move == "resize" and "META" or "CTRL",
+    action = w.action_callback(function(win, pane)
+      if is_vim(pane) then
+        -- pass the keys through to vim/nvim
+        win:perform_action({
+          SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+        }, pane)
+      else
+        if resize_or_move == "resize" then
+          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+        else
+          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+        end
+      end
+    end),
+  }
+end
+
+-- key mappings
+
+config.keys = {
+  -- move between split panes
+  split_nav("move", "h"),
+  split_nav("move", "j"),
+  split_nav("move", "k"),
+  split_nav("move", "l"),
+  -- resize panes
+  split_nav("resize", "h"),
+  split_nav("resize", "j"),
+  split_nav("resize", "k"),
+  split_nav("resize", "l"),
+}
+
+return config
